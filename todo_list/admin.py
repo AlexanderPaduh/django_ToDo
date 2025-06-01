7c3f6f4c9c7eb8e4c26f643fb5ba60a004f2bcc9
aiogram_bot <sanjapaduh@gmail.com>
2025-06-01 09:09:52 +0200

    upgrade admin

--- todo_list/admin.py
+++ todo_list/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from todo_list.models import ToDoItem
+
+@admin.register(ToDoItem)
+class ToDoItemAdmin(admin.ModelAdmin):
+  list_display = 'title','done'
+
+  def __str__(self):
+      return f"{self.title} - {self.done}"
--- todo_list/models.py
+++ todo_list/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
+
 class ToDoItem(models.Model):
-  title = models.CharField(max_length=250)
-  done = models.BooleanField(default=False)
+    class Meta:
+        verbose_name = "ToDo Item"
+
+    title = models.CharField(max_length=250)
+    done = models.BooleanField(default=False)
