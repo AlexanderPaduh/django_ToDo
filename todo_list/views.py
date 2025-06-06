641c0e3bb486979721198e5aad9ff08ae93ab1a1
aiogram_bot <sanjapaduh@gmail.com>
2025-06-06 22:28:28 +0200

    more work with models

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,29 +1,32 @@
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 from .models import ToDoItem
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView
 
 
 def index_view(request: HttpRequest) -> HttpResponse:
-    todo_items = ToDoItem.objects.all()
+    todo_items = ToDoItem.objects.all()[0:1]
     return render(request,
                   template_name="todo_list/index.html",
                   context={"todo_items": todo_items})
 
 
-class ToDoListIndexView(TemplateView):
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        return context
-        context ["todo_items"]: [todo_items] = ToDoItem.objects.all()
+class ToDoListIndexView(ListView):
     template_name = "todo_list/index.html"
+    queryset = ToDoItem.objects.all()[0:3]
+    #     context = super().get_context_data(**kwargs)
+    #     return context
+    #     context ["todo_items"]: [todo_items] = ToDoItem.object.all()
+
 
 
 class ToDoListView(ListView):
     # template_name = "todo_list/index.html"
     model = ToDoItem
     # context_object_name = "todo_items"
-    def get_context_data(self, **kwargs):
-        print(ToDoItem._meta.app_label)
-        print(ToDoItem._meta.model_name)
-        return super().get_context_data(**kwargs)
+
+class ToDoListDoneView(ListView):
+    queryset = ToDoItem.objects.filter(done=True).all()
+
+class ToDoDetailView(DetailView):
+    model = ToDoItem
