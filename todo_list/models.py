05cf867ef971b3d6b5c37f7434d6946c4d5656d6
aiogram_bot <sanjapaduh@gmail.com>
2025-06-11 12:11:07 +0200

    before bag

--- todo_list/models.py
+++ todo_list/models.py
@@ -3,19 +3,21 @@ from django.urls import reverse
 
 
 class ToDoItem(models.Model):
+
     class Meta:
-        ordering = ("id",)
+        ordering = ("id", )
         verbose_name = "ToDo Item"
 
     title = models.CharField(max_length=250)
     description = models.TextField(blank=True, null=False)
     done = models.BooleanField(default=False)
+    archived = models.BooleanField(default=False)
 
-
-
-    def get_absolute_url(self) -> str:
-        return reverse("todo_list:detail",
-                       kwargs={"pk": self.pk})
+    def get_absolute_url(self):
+        return reverse(
+            "todo_list:detail",
+            kwargs={"pk": self.pk},
+        )
 
     def __str__(self):
         return self.title
